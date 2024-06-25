f7fc7f62415bcf878afc5fff1c1dd40101453219
NBMARIASW11\Maria Stachowiak <mariastachowiak87@gmail.com>
2024-06-25 16:33:52 +0200

    remove password field from UserDto

--- src/main/java/com/petadoptionapp/petadoptionapp/mapper/UserMapper.java
+++ src/main/java/com/petadoptionapp/petadoptionapp/mapper/UserMapper.java
@@ -1,18 +1,28 @@
 package com.petadoptionapp.petadoptionapp.mapper;
 
 import com.petadoptionapp.petadoptionapp.bean.dao.User;
-import com.petadoptionapp.petadoptionapp.bean.dto.UserDto;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.MappingConstants;
+import org.openapitools.model.UserDto;
 
 @Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
 public interface UserMapper {
 
+    @Mapping(source = "id", target = "id")
+    @Mapping(source = "firstName", target = "firstName")
+    @Mapping(source = "lastName", target = "lastName")
+    @Mapping(source = "email", target = "email")
+    @Mapping(source = "phoneNumber", target = "phoneNumber")
+    @Mapping(source = "password", target = "password")
     User map(UserDto userDto);
 
+    @Mapping(source = "id", target = "id")
+    @Mapping(source = "firstName", target = "firstName")
+    @Mapping(source = "lastName", target = "lastName")
+    @Mapping(source = "email", target = "email")
+    @Mapping(source = "phoneNumber", target = "phoneNumber")
     @Mapping(target = "password", ignore = true)
     UserDto map(User user);
-
 
 }
